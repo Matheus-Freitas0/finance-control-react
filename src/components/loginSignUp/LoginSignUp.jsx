848cc698df5704d1df4e4cf0c1e14047e0177573
Matheus-Freitas0 <moncaomatheus@gmail.com>
2024-11-04 11:12:42 -0300

    Added imports for email, user, and password icons; reformatted code with consistent spacing and semicolons.

--- src/components/loginSignUp/LoginSignUp.jsx
+++ src/components/loginSignUp/LoginSignUp.jsx
@@ -1,13 +1,14 @@
+import email_icon from "../Assets/email.png";
+import user_icon from "../Assets/person.png";
+import password from "../Assets/password.png";
 import React from "react";
-import './LoginSignUp.css'
+import "./LoginSignUp.css";
 
-const LoginSignUp =() =>{
-    return(
-        <div>
-            <div className="login-signup">
-
-            </div>
-        </div>
-    )
-}
-export default LoginSignUp
+const LoginSignUp = () => {
+  return (
+    <div>
+      <div className="login-signup"></div>
+    </div>
+  );
+};
+export default LoginSignUp;
