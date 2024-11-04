05fb3fed7d52fc4b6bf996bda2d555f04e205780
MatheusFreitas <moncaomatheus@gmail.com>
2024-11-04 17:55:37 -0300

    Updated LoginSignUp component CSS and JSX: adjusted gap and margin in CSS, added onClick handlers and updated class names in JSX.

--- src/components/loginSignUp/LoginSignUp.jsx
+++ src/components/loginSignUp/LoginSignUp.jsx
@@ -39,8 +39,8 @@ function LoginSignUp() {
       </div>
 
       <div className="submit-container">
-        <div className={action ==='Login'?'submit gray':'submit'}> Sign Up</div>
-        <div className={action === "Sign Up"? "submit gray": 'submit'}> Login</div>
+        <div className={action ==='Login'?'submit gray':'submit'} onClick={()=>{setAction("Sign Up")}}> Sign Up</div>
+        <div className={action === "Sign Up"? "submit gray": 'submit'} onClick={()=>{setAction("Login")}}> Login</div>
       </div>
     </div>
   );
