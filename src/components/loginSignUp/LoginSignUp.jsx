811e927331273744b2b9462765083360823caa67
MatheusFreitas <moncaomatheus@gmail.com>
2024-11-04 18:19:25 -0300

    Updated LoginSignUp component: added new CSS styles, modified JSX structure, and changed button behavior

--- src/components/loginSignUp/LoginSignUp.jsx
+++ src/components/loginSignUp/LoginSignUp.jsx
@@ -1,12 +1,11 @@
 import email_icon from "../Assets/email.png";
 import user_icon from "../Assets/person.png";
 import password_icon from "../Assets/password.png";
-import React, { useState } from "react";
+import React, { act, useState } from "react";
 import "./LoginSignUp.css";
 
 function LoginSignUp() {
-
-  const [action, setAction] = useState('Sign Up')
+  const [action, setAction] = useState("Sign Up");
 
   return (
     <div className="container">
@@ -16,31 +15,59 @@ function LoginSignUp() {
       </div>
 
       <div className="inputs">
-        <div className="input">
-          <img src={user_icon} alt="user" />
-          <input type="text" placeholder="User" required/>
-        </div>
+        {action === "Login" ? (
+          <div></div>
+        ) : (
+          <div className="input">
+            <img src={user_icon} alt="user" />
+            <input type="text" placeholder="User" required />
+          </div>
+        )}
 
         <div className="input">
           <img src={email_icon} alt="email" />
-          <input type="email" placeholder="Email" required/>
+          <input type="email" placeholder="Email" required />
         </div>
 
         <div className="input">
           <img src={password_icon} alt="password" />
-          <input type="password" placeholder="Password" required/>
+          <input type="password" placeholder="Password" required />
         </div>
       </div>
-
-      <div className="forgot-password">
-        <p>
-          Lost password? <span>Click here</span>
-        </p>
-      </div>
+      {action === "Sign Up" ? (
+        <div></div>
+      ) : (
+        <div className="forgot-password">
+          <p>
+            Lost password? <span>Click here</span>
+          </p>
+        </div>
+      )}
 
       <div className="submit-container">
-        <div className={action ==='Login'?'submit gray':'submit'} onClick={()=>{setAction("Sign Up")}}> Sign Up</div>
-        <div className={action === "Sign Up"? "submit gray": 'submit'} onClick={()=>{setAction("Login")}}> Login</div>
+        <div
+          className={action === "Login" ? "submit gray" : "submit"}
+          onClick={() => {
+            setAction("Sign Up");
+          }}
+        >
+          Sign Up
+        </div>
+
+        <div
+          className={action === "Sign Up" ? "submit gray" : "submit"}
+          onClick={() => {
+            setAction("Login");
+          }}
+        >
+          Login
+        </div>
+      </div>
+      <div className="send">
+
+      <div className='sendButton' onClick={()=>{alert('Accessing the system...')}}>
+        Send
+      </div>
       </div>
     </div>
   );
