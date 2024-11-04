85560eaeeb55fb1428eba92120be763e85a0cf55
Matheus-Freitas0 <moncaomatheus@gmail.com>
2024-11-04 13:30:44 -0300

    Updated LoginSignUp component CSS and JSX files: added width and border-radius to container, modified margin and padding, added forgot-password class, and reformatted JSX code.

--- src/components/loginSignUp/LoginSignUp.jsx
+++ src/components/loginSignUp/LoginSignUp.jsx
@@ -4,43 +4,41 @@ import password_icon from "../Assets/password.png";
 import React from "react";
 import "./LoginSignUp.css";
 
-function LoginSignUp () {
+function LoginSignUp() {
   return (
     <div className="container">
-      
       <div className="header">
         <div className="text"> Sign up</div>
         <div className="underline"></div>
       </div>
 
       <div className="inputs">
-
         <div className="input">
           <img src={user_icon} alt="user" />
-          <input type="text" placeholder="User"/>
+          <input type="text" placeholder="User" />
         </div>
 
         <div className="input">
           <img src={email_icon} alt="email" />
-          <input type="email" placeholder="Email"/>
+          <input type="email" placeholder="Email" />
         </div>
 
         <div className="input">
           <img src={password_icon} alt="password" />
-          <input type="password" placeholder="Password"/>
+          <input type="password" placeholder="Password" />
         </div>
-      </div>
 
-      <div className="forgot-password">
-        Lost password? <span>Click here</span>
       </div>
+        
+        <div className="forgot-password">
+          Lost password? <span>Click here</span>
+        </div>
 
       <div className="submit-container">
         <div className="submit"> Sign Up</div>
         <div className="submit"> Login</div>
       </div>
-
     </div>
   );
-};
+}
 export default LoginSignUp;
