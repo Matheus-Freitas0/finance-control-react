28807a0dd28e2b33938c7b19a673fb0e4bef1b96
Matheus-Freitas0 <moncaomatheus@gmail.com>
2024-11-04 11:28:42 -0300

    Updated LoginSignUp component with new HTML structure and added input fields with icons

--- src/components/loginSignUp/LoginSignUp.jsx
+++ src/components/loginSignUp/LoginSignUp.jsx
@@ -6,8 +6,28 @@ import "./LoginSignUp.css";
 
 const LoginSignUp = () => {
   return (
-    <div>
-      <div className="login-signup"></div>
+    <div className="cointainer">
+      <div className="header">
+        <div className="text"> Sign up</div>
+        <div className="underline"></div>
+      </div>
+
+      <div className="inputs">
+        <div className="input">
+            <img src={email_icon} alt="email" />
+            <input type="text" />
+        </div>
+
+        <div className="input">
+            <img src={user_icon} alt="user" />
+            <input type="text" />
+        </div>
+
+        <div className="input">
+            <img src={password_icon} alt="password" />
+            <input type="text" />
+        </div>
+      </div>
     </div>
   );
 };
